fa4764111afa14725608df9f05d227905a807f58
johncpalmer <johnpalmer@Johns-MacBook-Pro.local>
2014-09-07 19:31:07 -0400

    moved things from app to public

--- app/views/index.php
+++ app/views/index.php
@@ -3,11 +3,11 @@
   <head>
     <title>Periscope</title>
     <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js"></script>
-    <script src="app/views/Dependencies/mustache.js"></script>
-    <script src="app/views/Dependencies/chevron.js"></script>
-    <script src="app/views/main.js"></script>
-    <link href="templates/landing.mustache" rel="template" id="landingPage"/>
-    <link rel="stylesheet" type="text/css" href="app/views/styles/main.css" />
+    <script src="public/views/Dependencies/mustache.js"></script>
+    <script src="public/views/Dependencies/chevron.js"></script>
+    <script src="public/views/main.js"></script>
+    <link href="public/templates/landing.mustache" rel="template" id="landingPage"/>
+    <link rel="stylesheet" type="text/css" href="public/views/styles/main.css" />
   </head>
   <body>
   </body>
