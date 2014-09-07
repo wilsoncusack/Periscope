74ebebf6435bb8dd432d92354919cc418ca7c742
johncpalmer <johnpalmer@Johns-MacBook-Pro.local>
2014-09-07 19:49:24 -0400

    tried echoing assets in php file

--- app/views/index.php
+++ app/views/index.php
@@ -3,11 +3,11 @@
   <head>
     <title>Periscope</title>
     <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js"></script>
-    <script src="views/Dependencies/mustache.js"></script>
-    <script src="views/Dependencies/chevron.js"></script>
-    <script src="views/main.js"></script>
-    <link href="templates/landing.mustache" rel="template" id="landingPage"/>
-    <link rel="stylesheet" type="text/css" href="views/styles/main.css" />
+    <script src="<?php echo asset('Dependencies/mustache.js')?>"></script>
+    <script src="<?php echo asset('Dependencies/chevron.js')?>"></script>
+    <script src="<?php echo asset('main.js')?>"></script>
+    <link href="<?php echo asset('templates/landing.mustache')?>" rel="template" id="landingPage"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo asset('styles/main.css')?>" />
   </head>
   <body>
   </body>
