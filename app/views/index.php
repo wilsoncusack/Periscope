1399531b23914f0ee368ef4ab883cce79f4df495
johncpalmer <johnpalmer@Johns-MacBook-Pro.local>
2014-09-07 20:14:39 -0400

    changed to capital T

--- app/views/index.php
+++ app/views/index.php
@@ -6,7 +6,7 @@
     <script src="<?php echo asset('Dependencies/mustache.js')?>"></script>
     <script src="<?php echo asset('Dependencies/chevron.js')?>"></script>
     <script src="<?php echo asset('main.js')?>"></script>
-    <link rel="template" id="landingPage" href="<?php echo asset('templates/landing.mustache')?>"/>
+    <link rel="template" id="landingPage" href="<?php echo asset('Templates/landing.mustache')?>"/>
     <link rel="stylesheet" type="text/css" href="<?php echo asset('styles/main.css')?>"/>
   </head>
   <body>
