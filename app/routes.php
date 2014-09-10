7b088daf5bee22a8b58f3ab8e93d6dfb5fa58c12
johncpalmer <johnpalmer@Johns-MacBook-Pro.local>
2014-09-09 21:43:32 -0400

    added logic to routing to make sure they entered a valid email and make sure it doesnt already exist in the DB.

--- app/routes.php
+++ app/routes.php
@@ -19,3 +19,19 @@ Route::get('/', function()
 Route::get('/DBTEST', function() {
 	DB::insert('insert into email_log (email, access) values (?, ?)', array("test1", "true"));
 });
+
+Route::post('/insertEmail', function($newEmail) {
+	if(empty(DB::select('select * from email_log where email = ?', array($newEmail)))) {
+		if(preg_match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', $newEmail)) {
+			if(DB::insert('insert into email_log (email,access) values (?, ?)', array($newEmail, 't'))) {
+				return Response::json(array('success' => 'Email has been entered'), 200);
+			} else {
+				return Response::json(array('error' => 'Insertion failed.'), 500);
+			}
+		} else {
+			return Response::json(array('error' => 'Not a valid email address.'), 400);
+		}
+	} else {
+		return Response::json(array('error' => 'Email already exists.'), 400);
+	}
+});
